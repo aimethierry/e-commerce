9c030ac97a6bb8168b72c8af5b3cb8ef7af621fc
aimethierry <aime.thierry97@gmail.com>
2018-04-11 16:58:55 +0200

    changed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,11 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%%e+3kl3a9ab_gb@%3qqr+de3!36^-25(7y&xuxlci!&$u6-pf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
-
 # Application definition
 
 INSTALLED_APPS = (
